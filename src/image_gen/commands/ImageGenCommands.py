db70def134b6259937705cd05b110d8676ad01ab
handamonium <kerrminater@gmail.com>
2025-03-06 19:44:29 -0500

    Add MAX_WIDTH option to the config for i2v wan2.1. The image dimensions won't exceed MAX_WIDTH. Added the prompt's text to the command message for /video commands.

--- src/image_gen/commands/ImageGenCommands.py
+++ src/image_gen/commands/ImageGenCommands.py
@@ -163,8 +163,8 @@ class ImageGenCommands:
             )
             await self._do_request(
                 interaction,
-                f"🎥{interaction.user.mention} asked me to create a video with WAN! {random.choice(generation_messages)} 🎥",
-                f"{interaction.user.mention} asked me to create video with WAN! {random.choice(completion_messages)} 🎥",
+                f'🎥{interaction.user.mention} asked me to imagine "{prompt}"! with WAN! {random.choice(generation_messages)} 🎥',
+                f'{interaction.user.mention} asked me to imagine "{prompt}" with WAN! {random.choice(completion_messages)} 🎥',
                 "wan",
                 params,
             )
